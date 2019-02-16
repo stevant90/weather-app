593f77225de51d0159bcf38e2f20f2a5d374412c
stevant90 <stevann90@gmail.com>
2019-02-16 12:38:21 +0100

    Updates

---
+++ precache-manifest.884981d7b7d44c589fb0d6aed4f112cf.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "e60f3277854b19c13c6b907ed0be130a",
+    "url": "/weather-app/static/media/ns.e60f3277.jpg"
+  },
+  {
+    "revision": "a0b216df72ec1ec24a62",
+    "url": "/weather-app/static/css/main.761a4349.chunk.css"
+  },
+  {
+    "revision": "19ff5fcc964c9487669d",
+    "url": "/weather-app/static/js/1.19ff5fcc.chunk.js"
+  },
+  {
+    "revision": "2be6f7590b391fa0dcfc",
+    "url": "/weather-app/static/js/runtime~main.2be6f759.js"
+  },
+  {
+    "revision": "9938a26aae167b2c34c68e5fa0486d96",
+    "url": "/weather-app/static/media/cloudy.9938a26a.svg"
+  },
+  {
+    "revision": "e2f5f1c58d25e1bf47ca366f112e9670",
+    "url": "/weather-app/static/media/sun.e2f5f1c5.svg"
+  },
+  {
+    "revision": "321d4a397b91576db7acd19149cca207",
+    "url": "/weather-app/static/media/bg.321d4a39.jpg"
+  },
+  {
+    "revision": "a0b216df72ec1ec24a62",
+    "url": "/weather-app/static/js/main.a0b216df.chunk.js"
+  },
+  {
+    "revision": "979e2bcc44ef4b7d546c9dda93c96314",
+    "url": "/weather-app/static/media/cloud.979e2bcc.svg"
+  },
+  {
+    "revision": "65c686c650991abf8e766af83e4b58b8",
+    "url": "/weather-app/static/media/rain.65c686c6.svg"
+  },
+  {
+    "revision": "b559aea6cdf0d0d27226511df41b5dcb",
+    "url": "/weather-app/static/media/storn.b559aea6.svg"
+  },
+  {
+    "revision": "326b62d13c511494f01a2cb222600a42",
+    "url": "/weather-app/static/media/snowing.326b62d1.svg"
+  },
+  {
+    "revision": "d033d080325bbb6425eecbfee3302f6d",
+    "url": "/weather-app/static/media/moon.d033d080.svg"
+  },
+  {
+    "revision": "72c5a2235b90bd102ded6771d3836532",
+    "url": "/weather-app/static/media/refresh.72c5a223.svg"
+  },
+  {
+    "revision": "f8f970148f76e851772126727021f838",
+    "url": "/weather-app/index.html"
+  }
+];
